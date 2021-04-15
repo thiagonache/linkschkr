d33857a5807746b0d1812c7c1b791af4cb1dc6fe
Thiago Nache Carvalho <thiagonbcarvalho@gmail.com>
2021-04-15 17:06:09 -0300

    Update cmd

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"links"
 	"log"
 )
 
 func main() {
-	lk := links.NewChecker([]string{"https://golang.org"})
-	if err := lk.Run(); err != nil {
+	lk := links.NewChecker("https://bitfieldconsulting.com/")
+	success, failures, err := lk.Run()
+	if err != nil {
 		log.Fatal(err)
+	}
+	fmt.Println("SUCCESS")
+	for _, s := range success {
+		fmt.Printf("Link: %s StatusCode: %d Error: %v\n", s.URL, s.StatusCode, s.Err)
+	}
+	fmt.Println()
+	fmt.Println("FAILURES")
+	for _, f := range failures {
+		fmt.Printf("Link: %s StatusCode: %d Error: %v\n", f.URL, f.StatusCode, f.Err)
 	}
 }
