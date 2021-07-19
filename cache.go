57f155c88b5d75786f01f08cd4a2b96dc5e3e042
Thiago Nache Carvalho <thiagonbcarvalho@gmail.com>
2021-07-19 16:03:41 -0300

    Add TTL for cache entries

--- cache.go
+++ cache.go
@@ -1,20 +1,38 @@
 package links
 
+import (
+	"time"
+)
+
 type Cache struct {
-	data map[string]string
+	data map[string]CacheItem
+	ttl  time.Duration
+}
+
+type CacheItem struct {
+	entry   string
+	expires time.Time
 }
 
 func NewCache() *Cache {
 	return &Cache{
-		data: map[string]string{},
+		data: map[string]CacheItem{},
+		ttl:  86400 * time.Second,
 	}
 }
 
 func (c *Cache) Get(key string) (string, bool) {
 	value, ok := c.data[key]
-	return value, ok
+	if value.expires.Sub(time.Now().UTC()) >= 0 {
+		return value.entry, ok
+	}
+	return "", false
 }
 
 func (c *Cache) Store(key, value string) {
-	c.data[key] = value
+	c.data[key] = CacheItem{entry: value, expires: time.Now().UTC().Add(c.ttl)}
+}
+
+func (c *Cache) SetTTL(n int) {
+	c.ttl = time.Duration(n) * time.Second
 }
--- cache_test.go
+++ cache_test.go
@@ -3,6 +3,7 @@ package links_test
 import (
 	"links"
 	"testing"
+	"time"
 )
 
 func TestGetCache(t *testing.T) {
@@ -21,5 +22,30 @@ func TestGetCache(t *testing.T) {
 	}
 	if want != got {
 		t.Errorf("want %q but got %q", want, got)
+	}
+}
+
+func TestGetCacheExpired(t *testing.T) {
+	t.Parallel()
+	url := "https://golang.org"
+	cache := links.NewCache()
+	cache.SetTTL(1)
+	got, ok := cache.Get(url)
+	if ok {
+		t.Fatalf("cache should empty but %q was found", got)
+	}
+	want := "johniscool"
+	cache.Store(url, want)
+	got, ok = cache.Get(url)
+	if !ok {
+		t.Fatal("fail to retrieve stored value")
+	}
+	if want != got {
+		t.Errorf("want %q but got %q", want, got)
+	}
+	time.Sleep(1 * time.Second)
+	got, ok = cache.Get(url)
+	if ok {
+		t.Fatalf("cache should empty but %q was found", got)
 	}
 }
